48390e0f59d2e3402286c5ca4cb1bb471b79d5de
arany677 <arany.cse.20220204053@aust.edu>
2026-02-11 00:45:32 +0600

    database problem solved

--- mrittika.Server/Program.cs
+++ mrittika.Server/Program.cs
@@ -17,5 +17,28 @@ app.UseStaticFiles();  // This allows the browser to see the wwwroot/uploads fol
 app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
+// Inside Program.cs, just before app.Run();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<mrittika.Server.Data.ApplicationDbContext>();
+
+    // Check if there are already blogs
+    if (!context.Blogs.Any())
+    {
+        context.Blogs.AddRange(
+            new mrittika.Server.Models.Blog
+            {
+                Title = "Team Post: Handcrafted Clay",
+                Content = "This is a shared post for the whole team to see.",
+                AuthorName = "System",
+                ImageUrl = "/hero1.jpg", // Use an image already in public/
+                IsApproved = true,
+                CreatedAt = DateTime.Now
+            }
+        );
+        context.SaveChanges();
+    }
+}
 
 app.Run();
